086be71f50b9aa20b26fbb756611e9b6fa452a2e
loftuser <loftuser@YosemiteTechBoot.local>
2017-04-27 13:28:58 +0200

    Initial commit for BOSurface Factory object

---
+++ src/IonMovers/BOSurfaceFactory.h
@@ -0,0 +1,17 @@
+#ifndef QMCPLUSPLUS_BO_SURFACE_FACTORY_H
+#define QMCPLUSPLUS_BO_SURFACE_FACTORY_H
+
+namespace qmcplusplus
+{
+class BOSurfaceFactory
+{
+  public:
+    BOSurfaceFactory();
+    ~BOSurfaceFactory();
+
+  private:
+
+};
+
+}
+#endif
