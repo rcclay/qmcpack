1f7763db493cdbcc9478ba05c90ece8b18066ea5
Cody Melton <cmelton@sandia.gov>
2026-01-07 11:59:32 -0700

    add empty test file

---
+++ src/QMCHamiltonians/tests/test_zvzb_stress.cpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source License.
+// See LICENSE file in top directory for details.
+//
+// Copyright (c) 2026 QMCPACK developers.
+//
+// File developed by:
+//
+//
+// File created by:
+//////////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+
+namespace qmcplusplus
+{
+
+TEST_CASE("Stress test", "[hamiltonian"])
+{}
+
+} // namespace qmcplusplus
